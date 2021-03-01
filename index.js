451e19a9e9187fbd9660898d6e434469ca08ceec
rcastaneda1491 <66752435+rcastaneda1491@users.noreply.github.com>
2021-03-01 08:02:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ window.addEventListener('load', () => {
 	grid.refreshItems().layout();
 	document.getElementById('grid').classList.add('imagenes-cargadas');
 
-	// Agregamos los listener de los enlaces para filtrar por categoria.
 	const enlaces = document.querySelectorAll('#categorias a');
 	enlaces.forEach((elemento) => {
 		elemento.addEventListener('click', (evento) => {
@@ -21,13 +20,11 @@ window.addEventListener('load', () => {
 		});
 	});
 
-	// Agregamos el listener para la barra de busqueda
 	document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
 		const busqueda = evento.target.value;
 		grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
 	});
 
-	// Agregamos listener para las imagenes
 	const overlay = document.getElementById('overlay');
 	document.querySelectorAll('.grid .item img').forEach((elemento) => {
 		elemento.addEventListener('click', () => {
@@ -40,12 +37,10 @@ window.addEventListener('load', () => {
 		});
 	});
 
-	// Eventlistener del boton de cerrar
 	document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
 		overlay.classList.remove('activo');
 	});
 
-	// Eventlistener del overlay
 	overlay.addEventListener('click', (evento) => {
 		evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
 	});
